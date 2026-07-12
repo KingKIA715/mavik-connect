b3499daa5dacf4b16a2d757ba61dd863f9c3087c
KingKIA715 <oneoff.kia.king@gmail.com>
2026-07-12 11:59:07 -0400

    Last Sync: 2026-07-12 11:59 (Mobile)

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,6 +1,1 @@
-export * from "./users";
-export * from "./groups";
-export * from "./groupMembers";
-export * from "./messages";
-export * from "./groupKeys";
-export * from "./dms";
+export * from "./schema";
