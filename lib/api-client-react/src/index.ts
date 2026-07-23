79f6f6e0f859b56bf810a2159b120a6e8be3a26f
KingKIA715 <oneoff.kia.king@gmail.com>
2026-07-23 18:00:10 -0400

    King Commit 2026-07-23 18:00

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -2,6 +2,5 @@ export * from "./generated/api";
 export * from "./generated/api.schemas";
 export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
 export type { AuthTokenGetter } from "./custom-fetch";
-export * from "./manual-key-backup";
 export * from './generated/api';
 export * from './generated/api.schemas';
